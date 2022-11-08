b257617b2bf8a0ba39ce855d9c18b3f5d2e5c13f
zxcursed1337 <olesya4u@gmail.com>
2022-11-08 12:38:34 +0200

    done

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-
+var input1=documentGetElementById("task1"),
+    res=documentGetElementById("result"),
+    button=documentGetElementById("button"),
+    counter=0;
+    button.addEventListener("click",
+    function(){
+        )
+        if(input1.value=="GitHub"){
+        counter++
+        console.log(counter);
+        button.setAttribute("disalled", "true")
+        }
+        else{
+        alert("Неправильно")
+        }
+        
+    }
+    
